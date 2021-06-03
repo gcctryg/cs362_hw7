0a8fd3fd47c8feb1bbf9f15bd263a6fd8fee1cd9
Xuelu Feng <gcctryg@gmail.com>
2021-06-02 22:29:53 -0700

    test case set up and fail

---
+++ test_q2.py
@@ -0,0 +1,13 @@
+import unittest
+import q2
+
+class TestCase(unittest.TestCase):
+    def test_leapYear(self):
+        self.assertEqual(q2.leapYear(4), "This is a leap year!")
+
+    def test_leapYear(self):
+        self.assertEqual(q2.leapYear(200), "This is a not leap year!")
+
+
+if __name__ == '__main__':
+    unittest.main()
